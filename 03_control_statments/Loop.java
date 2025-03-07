a83db8f1415b19f27acd0040e2aaa00d6ba6d0e1
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2025-03-07 12:15:46 +0600

    addded pattern theory slide and loop  problems

---
+++ 03_control_statments/Loop.java
@@ -0,0 +1,85 @@
+public class Loop {
+    public static void main(String[] args) {
+        /*
+         * problem 1:
+         * --------------------------------
+         * print the 1st 10 even numbers
+         */
+
+         /*
+          * problem 2:
+          * --------------------------------
+          * FizzBuzz problem
+          * print numbers from 1 to 100
+          * if the number is divisible by 3, print "Fizz"
+          * if the number is divisible by 5, print "Buzz"
+          * if the number is divisible by 3 and 5, print "FizzBuzz"
+          */
+
+          /*
+           * problem 3:
+           * --------------------------------
+           * print the following pattern for n=5
+           * # # # # #
+           * # # # # #
+           * # # # # #
+           * # # # # #
+           * # # # # #
+           */
+
+          /*
+           * problem 4:
+           * --------------------------------
+           * print the following pattern for n=5
+           * 1
+           * 1 2
+           * 1 2 3
+           * 1 2 3 4
+           * 1 2 3 4 5
+           */
+
+           /*
+            * problem 5:
+            * --------------------------------
+            * print the following pattern for n=5
+            * a
+            * a b
+            * a b c
+            * a b c d
+            * a b c d e
+            */
+
+            /*
+             * problem 6:
+             * --------------------------------
+             * print the following pattern for n=5
+             * x       x
+             *   x   x  
+             *     x    
+             *   x   x  
+             * x       x
+             */
+
+             /*
+              * problem 7:
+              * --------------------------------
+              * print the following pattern for n=5
+              * 1 2 3 4 5
+              * 1 2 3 4
+              * 1 2 3
+              * 1 2
+              * 1
+              */
+
+              /*
+               * problem 8:
+               * --------------------------------
+               * print the following pattern for n=5
+               * 1
+               * 2 3
+               * 4 5 6
+               * 7 8 9 10
+               * 11 12 13 14 15
+               */
+    }
+}
